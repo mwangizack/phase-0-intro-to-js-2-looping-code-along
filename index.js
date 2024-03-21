54a6c4c15b08e95c8056aadf5b43be50fbfd3815
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-03-21 20:16:56 +0300

    Create for and while loops in index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+    return gifts;
+}
+  
+  wrapGifts(gifts);
+
+  function writeCards(guests, event){
+    const inviteMessages = [];
+    for (let i = 0; i < guests.length; i++){
+        inviteMessages.push(`Thank you, ${guests[i]}, for the wonderful ${event} gift!`);
+    }
+    return inviteMessages;
+  }
+
+  const guestList = ["Nelson", "Cathie", "Clinton", "Brandy", "Valeria", "Esther"];
+
+  console.log(writeCards(guestList, "Birthday"));
+
+  function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        n--;
+    }
+  }
+
+  countDown(10);
